ace910efabd792a51f0e25b83027fe472fd6336e
tom creighton <tjcreighton@gmail.com>
2017-04-06 15:53:16 -0400

    fixing xapi uri

--- util/strings.js
+++ util/strings.js
@@ -1,5 +1,5 @@
 module.exports = {
-    xapi: 'https://w3id.org/xapi#',
+    xapi: 'https://w3id.org/xapi/ontology#',
     lrsstmt: 'https://lrs.adlnet.gov/xapi/statements/',
     rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
     foaf: 'http://xmlns.com/foaf/0.1/#',
